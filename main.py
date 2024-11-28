52c3d235c933022f4514a26746f12d3273f4b28c
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-28 18:57:54 +0500

    dch

---
+++ main.py
@@ -0,0 +1,77 @@
+# format satr
+name = "noor"
+
+print(f"my name is {name}")
+
+name = "noor"
+text = f"helo {name}"
+print(text)
+name = "ford"
+
+
+# arifmetiki
+
+a = 10
+b = 2
+print(a//b)
+
+n = 2
+m = 3
+print(n*m)
+
+x = 7
+y = 9
+print(x+y)
+
+k = 43
+l = 98
+print(l-m)
+
+h = 23
+g = 22
+print(h**g)
+
+
+# logiki
+
+a = 9
+b = 9
+print(a==b)
+
+a = 13
+b = 12
+print(a > b)
+
+a = 23
+b = 67
+print(a!=b)
+
+a = 45
+b = 98
+print(a<b)
+ 
+a = 22
+b = 76
+print(a<=b)
+
+a = 34
+b = 77
+print(b>=a)
+
+
+a = 9
+b = 6
+c = 90
+print(a>b and b<c)
+
+x = 88
+y = 99
+m = 77
+print(x<y or y>m)
+
+
+
+
+
+
+
